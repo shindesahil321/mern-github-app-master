be4c5d3524224ecf796d87f3d3c1792a2ce72008
shindesahil321 <162611635+shindesahil321@users.noreply.github.com>
2024-03-14 15:55:24 +0530

    Update connectMongoDB.js

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connectMongoDB() {
 	try {
-		await mongoose.connect(process.env.MONGO_URI);
+		await mongoose.connect("mongodb+srv://sahilshinde3377:ZevRlGfyP2Bgpq35@cluster0.hbe7sq8.mongodb.net/github-db?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("MONGODB connected");
 	} catch (error) {
 		console.log("Error connecting to MongoDB: ", error.message);
